28aec202dcfd200aee395946fd90d2e83297e939
Jason <jasonblue1626@gmail.com>
2020-07-03 00:42:32 -0500

    Fixed deepsource issues

--- pdfparse.py
+++ pdfparse.py
@@ -5,8 +5,7 @@ def parse_page(filepath, page_index):
     pdf = 'fst_undergraduate_handbook_2019-2020.pdf'
     with open(os.path.join(filepath, pdf),'rb') as handbook:
         read_handbook = PyPDF2.PdfFileReader(handbook)
-        number_of_pages = read_handbook.getNumPages()
-        page = read_handbook.getPage(page_index)
+        page_obj = read_handbook.getPage(page_index)
         page_content = page.extractText()
 
     return f"Page no. {page_index-3} {page_content}"
