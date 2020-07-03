efaf790ceb38cecb47094871be0fc51c3d5025f1
Jason <jasonblue1626@gmail.com>
2020-07-03 00:38:11 -0500

    Started parsing pdf

---
+++ pdfparse.py
@@ -0,0 +1,23 @@
+import os
+import PyPDF2
+import json
+
+def parse_page(filepath, page_index):
+    pdf = 'fst_undergraduate_handbook_2019-2020.pdf'
+    with open(os.path.join(filepath, pdf),'rb') as handbook:
+        read_handbook = PyPDF2.PdfFileReader(handbook)
+        number_of_pages = read_handbook.getNumPages()
+        page = read_handbook.getPage(page_index)
+        page_content = page.extractText()
+
+    return f"Page no. {page_index-3} {page_content}"
+
+
+if __name__ == '__main__':
+    with open('raw_handbook.txt','w+') as raw:
+        # Parse Biochem
+        for page in range(13,27):
+            data = parse_page('C:\\Users\\Andrew\\Desktop\\SciTechGuideBot',page)
+            raw.write(data)
+
+
---
+++ run.py
@@ -0,0 +1,22 @@
+# Python packages
+
+import os
+import sys
+
+# Importing user defined packages
+
+import config
+
+
+# Retrieve api object
+api_obj = config.api_connect()
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+   pass
