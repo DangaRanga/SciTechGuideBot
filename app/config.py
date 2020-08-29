f4b331b5178599092f13b783537b4078ad21e6d1
Jason <jasonblue1626@gmail.com>
2020-08-29 04:20:05 -0500

    Modularized code

---
+++ app/config.py
@@ -0,0 +1,32 @@
+"""Set up configuration for tweepy bot"""
+# Importing python related packages
+
+import os
+import tweepy
+
+def api_connect():
+    """Connects to the twitter API
+
+    Args:
+        None
+
+    Returns:
+        api: A twitter API Object
+
+    """
+    # Retrieve environment variables
+    CONSUMER_API_KEY = os.environ.get('CONSUMER_API_KEY') 
+    CONSUMER_API_SECRET_KEY = os.environ.get('CONSUMER_API_SECRET_KEY')
+    ACCESS_TOKEN= os.environ.get('ACCESS_TOKEN') 
+    ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
+    
+    # Authenticate to twitter
+    auth = tweepy.OAuthHandler(CONSUMER_API_KEY, CONSUMER_API_SECRET_KEY)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    
+    # Create API Object
+    api = tweepy.API(auth, 
+                    wait_on_rate_limit=True,
+                    wait_on_rate_limit_notify=True)
+    return api
+
